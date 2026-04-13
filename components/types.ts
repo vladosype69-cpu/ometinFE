419a9e8836b89667390f4da0982f3fc697b9a34e
vladosype69-cpu <vladosype69@gmail.com>
2026-04-13 21:43:51 +0200

    Update types.ts

--- components/types.ts
+++ components/types.ts
@@ -2,4 +2,8 @@ export interface Profile {
   id: number;
   name: string;
   image: string;
+  age?: number;        // voliteľné, ak niekde nemáš
+  city?: string;       // voliteľné
+  bio?: string;        // voliteľné
+  tags?: string[];     // voliteľné
 }
